f978c1cf735a2eb788962483e4c835c801585c23
Rpifer <Rpifer@udel.edu>
2015-02-22 14:41:31 -0500

    Added class

---
+++ QBRating/src/QuarterBackRating.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class QuarterBackRating {
+
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Passing Attempts: ");
+		double passingAttempts = input.nextDouble();
+		System.out.print("Enter the years to work: ");
+		double passCompletions = input.nextDouble();
+		
+		double passingYards = input.nextDouble();
+		
+		double touchdownPasses = input.nextDouble();
+		
+		double interceptions = input.nextDouble();
+	}
+}
